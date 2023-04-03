65ad1b3c097d90b1bc6afb9144250382fd240132
Samuela <115798007+koffi-samuela@users.noreply.github.com>
2023-04-03 20:58:21 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+function show(src) {
+  var div = document.getElementById("fullscreen");
+  div.style.display = "block";
+  div.style.backgroundImage = "url(" + src + ")";
+  
+}
+
+function hide() {
+  var div = document.getElementById("fullscreen");
+  div.style.display = "none";
+}
+
+
+                      
+
+
